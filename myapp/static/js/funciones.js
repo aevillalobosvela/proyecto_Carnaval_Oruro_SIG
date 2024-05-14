5b46c3820c0dc6b01861c8c3139fdcc742554196
Alvaro Villalobos Vela <vivealed@gmail.com>
2024-05-14 00:00:40 -0400

    update funciones.js, base.html, inicio.html and planifica.html

--- myapp/static/js/funciones.js
+++ myapp/static/js/funciones.js
@@ -1,2 +1,19 @@
 console.log("funciona");
 
+$(document).ready(function () {
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
+    $("#icon").toggleClass("fa-arrow-circle-left fa-arrow-circle-right");
+  });
+});
+
+var map = L.map("map").setView([-17.961255, -67.106084], 15);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([-17.961255, -67.106084])
+  .addTo(map)
+  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+  .openPopup();
