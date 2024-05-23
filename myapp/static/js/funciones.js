e3e14065048b0a0ada71cf17ef6fe3fe8d3e760e
Alvaro Villalobos Vela <vivealed@gmail.com>
2024-05-23 19:02:17 -0400

    update funciones.js, base.html and inicio.html

--- myapp/static/js/funciones.js
+++ myapp/static/js/funciones.js
@@ -1,4 +1,3 @@
-console.log("funciona");
 var marker = null;
 
 $(document).ready(function () {
@@ -17,22 +16,9 @@ var map1 = L.map("map1", {
   minZoom: 14,
 });
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map1);
-
-document
-  .getElementById("select-location")
-  .addEventListener("change", function (e) {
-    let coords = e.target.value.split(",");
-    map1.flyTo(coords, 17);
-    if (marker) {
-      map1.removeLayer(marker);
-    }
-    marker = L.marker(e.target.value.split(",")).addTo(map1);
-    markeroption = 1;
-  });
+L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+).addTo(map1);
 
 var osmLayer = new L.TileLayer(
   "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -62,21 +48,22 @@ var rec_carnaval = [
   [-17.957698858097284, -67.10479645507664],
   [-17.96469668892355, -67.10687022162124],
   [-17.968979296085056, -67.10805794178015],
-  [-17.97155501626077, -67.10880478849505],
-  [-17.970195654803433, -67.11436268976651],
-  [-17.969346773716726, -67.11412016972848],
-  [-17.969093423922775, -67.11491600040829],
-  [-17.969954496640337, -67.11520016457632],
-  [-17.969191749928235, -67.11788726049843],
-  [-17.968340812675248, -67.11756653470802],
+  [-17.971660934457464, -67.1088268299442],
+  [-17.970104038282503, -67.11445157524199],
+  [-17.969322327621455, -67.11420033430277],
+  [-17.96907055246133, -67.11499498440033],
+  [-17.9699337237225, -67.11526711673427],
+  [-17.969099621642712, -67.11786137691192],
+  [-17.968254722820056, -67.1175735136206],
   [-17.968051125084976, -67.11806238908132],
-  [-17.9672320383532, -67.11782994692255],
-  [-17.96705558543468, -67.11808110926447],
+  [-17.96717328392045, -67.11781577280718],
+  [-17.9670938382334, -67.11808266361845],
+  [-17.967109421355303, -67.11827014304588],
   [-17.967469390470484, -67.11855552699116],
 ];
 
 var polylineOptions = {
-  color: "red", // Color de la línea
+  color: "orange", // Color de la línea
   weight: 8, // Ancho de la línea en píxeles
   opacity: 0.7, // Opacidad de la línea
   lineCap: "round", // Estilo del final de la línea
@@ -89,22 +76,24 @@ var carnaval = L.polyline(rec_carnaval, polylineOptions).addTo(map1);
 
 var saludIcon = L.divIcon({
   html: '<i class="fa fa-plus-square fa-2x"></i>',
-  className: 'salud-icon'
+  className: "salud-icon",
 });
 
-var seguridadIcon = L.divIcon({
-  html: '<i class="fa fa-person-military-pointing fa-2x"></i>',
-  className: 'seguridad-icon'
+var seguridadIcon = L.icon({
+  iconUrl: "static/img/mapa/logo_policia.png", // Ruta relativa a la imagen local
+  iconSize: [38, 38], // Tamaño del icono [ancho, alto]
+  iconAnchor: [19, 38], // Punto del icono que se corresponderá con la posición del marcador
+  popupAnchor: [0, -38],
 });
 
 var depositoIcon = L.divIcon({
   html: '<i class="fa fa-dumpster fa-2x"></i>',
-  className: 'deposito-icon'
+  className: "deposito-icon",
 });
 
 var pasoIcon = L.divIcon({
-  html: '<i class="fa fa-road-barrier fa-2x"></i>',
-  className: 'paso-icon'
+  html: '<span class="fa-stack fa-lg"><i class="fa fa-circle-o fa-stack-2x"></i><i class="fa fa-walking fa-stack-1x"></i>',
+  className: "paso-icon",
 });
 
 var salud = [
@@ -128,10 +117,26 @@ var salud = [
 ];
 
 var seguridad = [
-  [-17.960904600543504, -67.10464288776342],
-  [-17.96997219875942, -67.10608421753413],
-  [-17.97004282806646, -67.10925108756975],
-  [-17.969577303263794, -67.11562158873598],
+  {
+    coord: [-17.960904600543504, -67.10464288776342],
+    popupContent: "<b>Estación de Policía A</b><br>Dirección: Calle Falsa 123",
+    imagen_ruta: "static/img/diablada1.png",
+  },
+  {
+    coord: [-17.96997219875942, -67.10608421753413],
+    popupContent: "<b>Estación de Policía B</b><br>Dirección: Caalsa 123",
+    imagen_ruta: "static/img/diablada2.png",
+  },
+  {
+    coord: [-17.97004282806646, -67.10925108756975],
+    popupContent: "<b>Estación de Policía C</b><br>Dirección: Callsa 123",
+    imagen_ruta: "static/img/diablada3.png",
+  },
+  {
+    coord: [-17.969577303263794, -67.11562158873598],
+    popupContent: "<b>Estación de Policía D</b><br>Dirección: Cala 123",
+    imagen_ruta: "static/img/diablada1.png",
+  },
 ];
 
 var deposito = [
@@ -154,22 +159,22 @@ var deposito = [
 ];
 
 var paso = [
-  [-17.960457160365724, -67.10559877720537],
+  [-17.960267941563636, -67.10566629779373],
   [-17.961438635889948, -67.10597809898691],
-  [-17.96236237096946, -67.10617534702816],
-  [-17.963661366993843, -67.10653949819036],
+  [-17.96232490570489, -67.10624536653067],
+  [-17.963638247407577, -67.10661875207893],
   [-17.96469277359838, -67.10692629832128],
-  [-17.96765412046922, -67.10757700390268],
-  [-17.969413910067992, -67.10810010779102],
-  [-17.97023701919334, -67.10840901416204],
-  [-17.971656969282176, -67.1088683395914],
-  [-17.970845512433478, -67.11182823593387],
-  [-17.970663461980536, -67.11261928320972],
-  [-17.970663461882392, -67.11263204204953],
-  [-17.97038431574108, -67.11351239986266],
-  [-17.969354813072535, -67.11410084359146],
-  [-17.96912309779381, -67.11493987163098],
-  [-17.967276180927286, -67.11778204170197],
+  [-17.96760141424165, -67.107748519253],
+  [-17.96931290786419, -67.10821900347709],
+  [-17.970145425340334, -67.10849503765594],
+  [-17.971549712202382, -67.10889715033233],
+  [-17.970761641932842, -67.11191130908375],
+  [-17.970600745648813, -67.1127185622055],
+  [-17.970291361872633, -67.11368578897864],
+  [-17.969290201345903, -67.11425703612471],
+  [-17.969038883539426, -67.11503249077249],
+  [-17.969006313356555, -67.11793503846157],
+  [-17.967101796362787, -67.1178681017945],
 ];
 
 var saludMarkers = L.layerGroup();
@@ -180,19 +185,34 @@ var pasoMarkers = L.layerGroup();
 // Añadir marcadores a las capas correspondientes
 
 salud.forEach(function (coord) {
-  L.marker(coord, { icon: saludIcon }).bindPopup('Puesto de salud').addTo(saludMarkers);
+  L.marker(coord, { icon: saludIcon })
+    .bindPopup("Puesto de salud")
+    .addTo(saludMarkers);
 });
 
-seguridad.forEach(function (coord) {
-  L.marker(coord, { icon: seguridadIcon }).bindPopup("Puesto policial").addTo(seguridadMarkers);
+seguridad.forEach(function (data) {
+  var mark_seguridad = L.marker(data.coord, { icon: seguridadIcon })
+    .bindPopup(data.popupContent)
+    .addTo(seguridadMarkers);
+
+  mark_seguridad.on("click", function () {
+    document.getElementById("marker-info").innerHTML = data.popupContent;
+    document.getElementById("marker-image").src = data.imagen_ruta;
+    var card = document.getElementById('card-info');
+    card.style.display = 'block'; 
+  });
 });
 
 deposito.forEach(function (coord) {
-  L.marker(coord, { icon: depositoIcon }).bindPopup("Deposito de basura").addTo(depositoMarkers);
+  L.marker(coord, { icon: depositoIcon })
+    .bindPopup("Deposito de basura")
+    .addTo(depositoMarkers);
 });
 
 paso.forEach(function (coord) {
-  L.marker(coord, { icon: pasoIcon }).bindPopup("Paso peatonal").addTo(pasoMarkers);
+  L.marker(coord, { icon: pasoIcon })
+    .bindPopup("Paso peatonal")
+    .addTo(pasoMarkers);
 });
 
 // Añadir las capas de marcadores al mapa
@@ -206,7 +226,7 @@ var overlayMaps = {
   "Puestos de salud": saludMarkers,
   "Puestos de policia": seguridadMarkers,
   "Depositos de basura": depositoMarkers,
-  "Paso peatonal": pasoMarkers
+  "Paso peatonal": pasoMarkers,
 };
 
-L.control.layers(null, overlayMaps).addTo(map1);
+L.control.layers(null, overlayMaps, {collapsed: false}).addTo(map1);
